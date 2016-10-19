66a34cd4445d733ca60f1f64189e5badb779385c
Reinaldo Díaz Lugo <reinaldo122@gmail.com>
2016-10-19 12:04:23 -0400

    Renamed controllers to follow slug-name standard for file names

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -36,36 +36,6 @@ angular.module('axpress')
 
 }]);;
 
-/**
- * @class RegisterController
-
- * @constructor register
-
- * @function doRegister
- *
- */
-angular.module('axpress')
-.controller('RegisterController',['$scope','Client',function($scope,Client){
-	$scope.users={
-		name:"test",
-		pass:"12345",
-		email:"youremail@gmail.com",
-		phone:"56-555-5555",
-	};
-	$scope.doRegister=function(registerForm){
-		if(registerForm.$valid){
-			alert("Thanks user "+JSON.stringify($scope.users));
-			Client.register($scope.users.email,$scope.users.pass,$scope.users.name)
-					.then(function(data){
-						console.log(data);
-					},function(error){
-						console.war("error...");
-						console.log(error);
-					});
-		}
-	};
-}]);;
-
 angular.module('axpress')
 .controller('LoginController', ['$scope', '$rootScope', 'Client', '$ionicPopup',
 function($scope, $rootScope, Client, $ionicPopup){
@@ -126,4 +96,34 @@ function($scope, $rootScope, Client, $ionicPopup){
     $scope.facebookAvailable = $rootScope.facebookLoaded;
 
     
+}]);;
+
+/**
+ * @class RegisterController
+
+ * @constructor register
+
+ * @function doRegister
+ *
+ */
+angular.module('axpress')
+.controller('RegisterController',['$scope','Client',function($scope,Client){
+	$scope.users={
+		name:"test",
+		pass:"12345",
+		email:"youremail@gmail.com",
+		phone:"56-555-5555",
+	};
+	$scope.doRegister=function(registerForm){
+		if(registerForm.$valid){
+			alert("Thanks user "+JSON.stringify($scope.users));
+			Client.register($scope.users.email,$scope.users.pass,$scope.users.name)
+					.then(function(data){
+						console.log(data);
+					},function(error){
+						console.war("error...");
+						console.log(error);
+					});
+		}
+	};
 }]);
