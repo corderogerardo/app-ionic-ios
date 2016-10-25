3212c2c1833c9b06983e92a4790d1319d1b580dd
Reinaldo Díaz Lugo <reinaldo122@gmail.com>
2016-10-24 22:10:39 -0400

    Rating service Service to qualify service on a given shipping

--- www/js/services.js
+++ www/js/services.js
@@ -432,6 +432,39 @@ angular.module('axpress')
 }]);;
 
 angular.module('axpress')
+.factory('Rating', RatingService);
+
+RatingService.$inject = ['$rootScope', '$q', 'Service'];
+
+function RatingService ($rootScope, $q, Service){
+    var service = new Service('/rating');
+
+    //Public Functions
+    service.post = post;
+
+    return service;
+
+    /**
+     * Registers a service rating
+     *
+     * @param      {Integer}   shippingId   The shipping identifier
+     * @param      {Integer}   rating       The rating
+     * @param      {String}    description  The description
+     * 
+     * @return     {Promise}  A promise to resolve results
+     */
+    function post (shippingId, rating, description) {
+        var data = {
+            shipping_id: shippingId,
+            rating: rating,
+            description: description
+        };
+
+        return service.apiPost('/post', data);
+    }
+};
+
+angular.module('axpress')
 .factory('Service', ['$http', 'constants', '$q', '$httpParamSerializerJQLike',
 function($http, constants, $q, $httpParamSerializerJQLike){
 
