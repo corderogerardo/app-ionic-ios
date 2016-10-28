3c715827cd5c0322eaa225aa25f7330c0d53fd6d
Reinaldo Díaz Lugo <reinaldo122@gmail.com>
2016-10-28 11:04:08 -0400

    Fix to address autocomplete not working on mobile
    Implemented fix to address autocomplete not working on mobile.
    Based on this issue: https://github.com/driftyco/ionic/issues/6387

--- src/js/directives/mapAutocompleteAddress.js
+++ src/js/directives/mapAutocompleteAddress.js
@@ -17,6 +17,30 @@
                     $scope.restrictions = { country: 'col' };
                     $scope.types = "['address']";
                 }
+
+                $scope.disableTap = function (event) {
+                    var input = event.target;
+
+                    // Get the predictions element
+                    var container = document.getElementsByClassName('pac-container');
+                    container = angular.element(container);
+
+                    // Apply css to ensure the container overlays the other elements, and
+                    // events occur on the element not behind it
+                    container.css('z-index', '10000');
+                    container.css('pointer-events', 'auto');
+
+                    // Disable ionic data tap
+                    container.attr('data-tap-disabled', 'true');
+
+                    // Leave the input field if a prediction is chosen
+                    container.on('click', function(){
+                        input.blur();
+                    });
+                };
+            },
+            
+            link: function (scope, element, attributes) {
             }
         };
     }
