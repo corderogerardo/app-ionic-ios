3b6238896e867fd38f6bc710044759e3dd634d9e
Reinaldo Díaz Lugo <reinaldo122@gmail.com>
2016-11-30 15:52:24 -0400

    Fixes to shipping flows with sidebar menu
    Fixed some steps on the shipping flows who where beign conflicting with the abstract state using sidebar menu

--- src/js/directives/sidebarMenu.js
+++ src/js/directives/sidebarMenu.js
@@ -11,14 +11,43 @@
         };
     }
 
-    sidebarMenuController.$inject = ['$rootScope', '$scope', 'Client'];
+    sidebarMenuController.$inject = ['$rootScope', '$scope', 'Client', '$ionicSideMenuDelegate'];
 
-    function sidebarMenuController ($rootScope, $scope, Client) {
+    function sidebarMenuController($rootScope, $scope, Client, $ionicSideMenuDelegate) {
         $scope.logout = logout;
+        $scope.isHome = isHome;
+        $scope.cancelService = cancelService;
+        $scope.isServiceActive = isServiceActive;
 
-        function logout () {
+        function logout() {
             Client.logout();
             $rootScope.$state.go('root');
+        }
+
+        function isHome() {
+            return $rootScope.$state.current.name == "app.main";
+        }
+
+        function cancelService() {
+            $rootScope.$state.get('app.document').data.data = {};
+            $rootScope.$state.get('app.package').data.data = {};
+            $rootScope.$state.get('app.diligence').data.data = {};
+            if ($rootScope.$state.current.name != "app.main") {
+                $rootScope.$state.go('app.main');
+                $ionicSideMenuDelegate.toggleLeft();
+            }
+        }
+
+        function isServiceActive() {
+            return !isEmpty($rootScope.$state.get('app.document').data.data) ||
+                !isEmpty($rootScope.$state.get('app.package').data.data) ||
+                !isEmpty($rootScope.$state.get('app.diligence').data.data);
+        }
+
+        function isEmpty(obj) {
+            for (var i in obj)
+                if (obj.hasOwnProperty(i)) return false;
+            return true;
         }
     }
 })();
