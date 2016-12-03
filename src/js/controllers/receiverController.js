8f227119f2d37d7a2ae549b1484ec81c0b19a20f
Reinaldo Díaz Lugo <reinaldo122@gmail.com>
2016-12-03 11:33:41 -0400

    Added simple validation on receiver
    Added pattern on phone field, and toast message to indicate required fields

--- src/js/controllers/receiverController.js
+++ src/js/controllers/receiverController.js
@@ -2,25 +2,31 @@
     angular.module('axpress')
         .controller('ReceiverController', ReceiverController);
 
-    ReceiverController.$inject = ['$rootScope', '$scope', '$state'];
+    ReceiverController.$inject = ['$rootScope', '$scope', '$state', 'Logger'];
 
-    function ReceiverController($rootScope, $scope, $state) {
+    function ReceiverController($rootScope, $scope, $state, Logger) {
         activate();
 
         $scope.saveCaracteristics = function() {
-            if ($scope.extraData.navigateTo) {
-                $state.go($scope.extraData.navigateTo);
-                delete $scope.extraData.navigateTo;
+            if (isFormValid()) {
+                if ($scope.extraData.navigateTo) {
+                    $state.go($scope.extraData.navigateTo);
+                    delete $scope.extraData.navigateTo;
+                } else {
+                    $state.go($scope.extraData.receiverNext);
+                }
             } else {
-                $state.go($scope.extraData.receiverNext);
+                Logger.toast("Debe completar el nombre, correo electrónico y teléfono");
             }
         };
 
+        function isFormValid () {
+            return $scope.data.destinyName != "" &&
+                $scope.data.emailDestinyClient != "" &&
+                $scope.data.cellphoneDestinyClient != "";
+        }
+
         function activate() {
-            $scope.focusedReceiverEmail = false;
-            $scope.focusedReceiverName = false;
-            $scope.focusedReceiverPhone = false;
-            $scope.focusedReceiverCI = false;
             $scope.data = $state.current.data.data;
             $scope.extraData = $state.current.data.extraData;
 
